e2da15a7726daad31d1f979d6e684e1d6dc89fbd
dojin <djsong33@gmail.com>
2025-03-26 12:43:27 +0900

    key 추가

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -7,7 +7,7 @@ export const payment = async(req, res) => {
     try {
         console.log(req.body);
         const {id, item_name, total_amount} = req.body;
-        const KAKAO_ADMIN_KEY = "3503fec683688808f60829560bee3343";
+        const KAKAO_ADMIN_KEY = "6494858726aa8879ead7931df5ec5092";
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
 
         const response = await axios.post(
